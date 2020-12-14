3e6f92316b4e33689bf557564c4b4d0b1a902495
Kelsey Cox <kelsey.cox@slalom.com>
2020-12-14 06:39:01 -0800

    forgot chromedriver

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 var { Builder, By, Key, until} = require('selenium-webdriver');
-var chrome = require('selenium-webdriver/chrome');
+require('chromedriver');
+// var chrome = require('selenium-webdriver/chrome');
 // var edge = require('selenium-webdriver/edge');
-var path = '/Users/kelsey.cox/Code/Selenium/Drivers/chromedriver';
+// var path = '/Users/kelsey.cox/Code/Selenium/Drivers/chromedriver';
 
 //let service = new edge.ServiceBuilder("/Users/kelsey.cox/Code/Selenium/Drivers/msedgedriver.exe");
-var service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
+// var service = new chrome.ServiceBuilder(path).build();
+// chrome.setDefaultService(service);
 
 async function searchTextOnGoogle() {
   var driver = new Builder().forBrowser('chrome').build();
