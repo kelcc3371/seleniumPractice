7fbcb4e19ab5de76746edc3d0c1a719d4f94d020
Kelsey Cox <kelsey.cox@slalom.com>
2020-12-14 06:51:45 -0800

    playing with jenkinsfile

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var webdriver = require('selenium-webdriver');
 
 async function searchTextOnGoogle() {
   driver = new webdriver.Builder().forBrowser('chrome').build();
-driver.manage().window().maximize();
+  driver.manage().window().maximize();
 
   driver.get('https://www.google.com');
   console.log('hello');
