5fa8a12b57c536da986cea4ade03412dee51b6cc
Kelsey Cox <kelsey.cox@slalom.com>
2020-12-14 07:14:49 -0800

    chrome

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var chrome = require('./node_modules/chromedriver');
 var webdriver = require('./node_modules/selenium-webdriver');
 
 
-var service = chrome.ServiceBuilder('path/to/chromedriver').build();
+var service = new chrome.ServiceBuilder('path/to/chromedriver').build();
 chrome.setDefaultService(service);
   
   
